c346454ec35c70ccde442dd207aaa54862e32ffa
Ivan Puljak <ivan@puljak.ca>
2020-03-25 17:42:46 -0400

    spacing

--- fcc_algorithms/2020/javascript/fcc_basic.js
+++ fcc_algorithms/2020/javascript/fcc_basic.js
@@ -3,10 +3,12 @@ function convertToF(celsius) {
     return celsius * 9 / 5 + 32;
 }
 
+
 // Reverse a String
 function reverseString(str) {
     return str.split('').reverse().join('');
 }
+
 
 // Factorialize a Number
 function factorialize(num) {
@@ -14,10 +16,12 @@ function factorialize(num) {
     return num * factorialize(num - 1);
 }
 
+
 // Find the Longest Word in a String - #1
 function findLongestWordLength(str) {
     return Math.max(...str.split(' ').map(x => x.length));
 }
+
 
 // Find the Longest Word in a String - #2
 function findLongestWordLength(str) {
@@ -26,15 +30,18 @@ function findLongestWordLength(str) {
     }, '').length;
 }
 
+
 // Return Largest Numbers in Arrays
 function largestOfFour(arr) {
     return arr.reduce((a, b) => a.concat(Math.max(...b)), []);
 }
 
+
 // Confirm the Ending
 function confirmEnding(str, target) {
     return str.slice(str.length - target.length) === target;
 }
+
 
 // Repeat a String Repeat a String - #1
 function repeatStringNumTimes(str, num) {
@@ -42,15 +49,18 @@ function repeatStringNumTimes(str, num) {
     return [...Array(num).keys()].reduce((a, b) => a + str, '');
 }
 
+
 // Repeat a String Repeat a String - #2
 function repeatStringNumTimes(str, num) {
     return num > 0 ? str.repeat(num) : '';
 }
 
+
 // Truncate a String
 function truncateString(str, num) {
     return str.length > num ? str.slice(0, num) + '...' : str;
 }
+
 
 // Finders Keepers
 function findElement(arr, func) {
@@ -61,10 +71,12 @@ function findElement(arr, func) {
     return;
 }
 
+
 // Boo who
 function booWho(bool) {
     return typeof bool === "boolean";
 }
+
 
 // Title Case a Sentence - #1
 function titleCase(str) {
@@ -75,20 +87,24 @@ function titleCase(str) {
     }).join(' ');
 }
 
+
 // Title Case a Sentence - #2
 function titleCase(str) {
     return str.toLowerCase().split(' ').map(w => w.replace(w[0], w[0].toUpperCase())).join(' ');
 }
+
 
 // Slice and Splice
 function frankenSplice(arr1, arr2, n) {
     return arr2.slice(0, n).concat(arr1).concat(arr2.slice(n));
 }
 
+
 // Falsy Bouncer
 function bouncer(arr) {
     return arr.reduce((a, b) => b ? a.concat([b]) : a, []);
 }
+
 
 // Where do I Belong
 function getIndexToIns(arr, num) {
@@ -101,10 +117,12 @@ function getIndexToIns(arr, num) {
     return sorted.length;
 }
 
+
 // Mutations
 function mutation(arr) {
     return arr[1].toLowerCase().split('').reduce((a, b) => a && arr[0].toLowerCase().includes(b), true);
 }
+
 
 // Chunky Monkey
 function chunkArrayInGroups(arr, size) {
