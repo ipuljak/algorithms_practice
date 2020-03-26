e06043168c9689f60a4401f51ffef2e4e08cbc41
Ivan Puljak <ivan@puljak.ca>
2020-03-26 15:49:17 -0400

    2020 fcc advanced done

--- fcc_algorithms/2020/javascript/fcc_advanced.js
+++ fcc_algorithms/2020/javascript/fcc_advanced.js
@@ -70,3 +70,111 @@ function convertToRoman(num) {
 
     return str;
 }
+
+
+// Caesars Cipher
+function rot13(str) {
+    function shiftLetter(letter) {
+        if (/[\W_]/.test(letter)) return letter;
+        const code = letter.charCodeAt(0);
+        const shifted = code + (code > 77 ? -13 : 13);
+        return String.fromCharCode(shifted);
+    }
+
+    return str
+        .split('')
+        .map(shiftLetter)
+        .join('');
+}
+
+
+// Telephone Number Validator
+function telephoneCheck(str) {
+    const country = /^((\(1\))|1)?[\s-]?/;
+    const area = /((\(\d{3}\))|\d{3})[\s-]?/;
+    const number = /\d{3}[\s-]?\d{4}$/;
+    const regex = new RegExp(country.source + area.source + number.source, "g");
+    return regex.test(str);
+}
+
+
+// Cash Register
+function checkCashRegister(price, cash, cid) {
+    // Function to help add decimals
+    const addDecimals = (a, b) => parseFloat((a + b).toFixed(2));
+
+    // Function to calculate how much change is available
+    const countBank = _ => bank.reduce((a, b) => addDecimals(a, b[1]), 0);
+
+    // Function to find the closest change match to the current diff
+    const findChange = diff => {
+        for (let x = bank.length - 1; x >= 0; x--) {
+            if (diff >= map[bank[x][0]] && bank[x][1] > 0) {
+                return bank[x][0];
+            }
+        }
+    }
+
+    // Function to update the change & bank by changing the given value amount
+    const addChange = val => {
+        const changeIndex = change.findIndex(x => x[0] === val);
+        const bankIndex = bank.findIndex(x => x[0] === val);
+
+        if (changeIndex >= 0) {
+            change[changeIndex][1] = addDecimals(change[changeIndex][1], map[val]);
+        } else {
+            change.push([val, map[val]]);
+        }
+
+        bank[bankIndex][1] = addDecimals(bank[bankIndex][1], -map[val]);
+    }
+
+    // Variables
+    let diff = addDecimals(cash, -price);
+    let bank = JSON.parse(JSON.stringify(cid));
+    let change = [];
+
+    // Value mapping
+    const map = {
+        PENNY: 0.01,
+        NICKEL: 0.05,
+        DIME: 0.1,
+        QUARTER: 0.25,
+        ONE: 1,
+        FIVE: 5,
+        TEN: 10,
+        TWENTY: 20,
+        'ONE_HUNDRED': 100
+    };
+
+    // Return empty change (exact amount was given) - OPEN
+    if (diff === 0) return { status: "OPEN", change: [] }
+
+    // Return empty change - INSUFFICIENT FUNDS
+    if (countBank(bank) - diff < 0) {
+        return { status: "INSUFFICIENT_FUNDS", change: [] };
+    }
+
+    // Count down the difference
+    while (diff !== 0) {
+        let match = findChange(diff);
+
+        if (!match) {
+            return {
+                status: "INSUFFICIENT_FUNDS",
+                change: []
+            };
+        }
+
+        addChange(match);
+        diff = addDecimals(diff, -map[match]);
+    }
+
+    // All change used up - CLOSED
+    if (bank.filter(x => x[1] === 0).length === bank.length) {
+        return { status: "CLOSED", change: cid };
+    }
+
+    // Return the change - OPEN
+    return { status: "OPEN", change };
+}
