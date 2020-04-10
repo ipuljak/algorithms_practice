3a246bfead698a808c724b36078a8640053c691a
Ivan Puljak <ivan@puljak.ca>
2020-04-10 17:31:35 -0400

    Leetcode easy practice

--- leetcode/2020/easy.js
+++ leetcode/2020/easy.js
@@ -1310,3 +1310,77 @@ var isPowerOfTwo = function (n) {
 var isPowerOfTwo = function (n) {
     return Math.log2(n) % 1 === 0;
 };
+
+
+/** 
+ * 232. Implement Queue using Stacks
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.stack = [];
+    this.front = null;
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack.push(x);
+    
+    if (!this.front) {
+        this.front = x;
+    }
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    let temp = [];
+    
+    while (this.stack.length) {
+        temp.push(this.stack.pop());
+    }
+    
+    let ans = temp.pop();
+
+    // Reset front value
+    this.front = temp.length ? temp[temp.length - 1] : null;
+    
+    // Reset the stack
+    while (temp.length) {
+        this.stack.push(temp.pop());
+    }
+    
+    return ans;
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.front;
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return !this.stack.length
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+
+
